bee8a7f2f01a4cd1fe94667a7dfd3d03596fbf56
pfrenyo <pfrenyo@outlook.com>
2019-06-16 23:06:45 +0200

    moved a typecast to earlier

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@
 # These type of variables will have a '# Dual use variable' comment before they're used.
 ADMIN_ID = "data/keys/admin.key"
 with open(ADMIN_ID, 'r') as f:
-    ADMIN_ID = f.readline()
+    ADMIN_ID = int(f.readline())
 
 
 # Function checking whether or not a user is the administrator of the bot.
@@ -16,7 +16,7 @@ with open(ADMIN_ID, 'r') as f:
 # @pre: the Admin object, the command/message's context
 # @post: boolean value indicating whether or not the sender is admin
 def is_admin(context):  # Can be done with a sexy decorator later: TO BE IMPLEMENTED!!!
-    return int(context.author.id) == int(ADMIN_ID)
+    return context.author.id == ADMIN_ID
 
 
 # Function fetching the discord user object of the admin of the server.
